6b5d89c6d8789d1e2202bb608d65b31b3e5b45a3
Rajeshg97 <rajeshg.gmx@gmail.com>
2021-08-14 22:57:48 +0530

    Update Environments.ts

--- Utils/Configs/Environments.ts
+++ Utils/Configs/Environments.ts
@@ -1,7 +1,7 @@
 export default class Env{
 
-    public static staging2 : string = "https://staging2.mystartupequity.com/login";
-    public static staging3 : string = "https://staging3.mystartupequity.com/login";
-    public static production : string = "https://mystartupequity.com/login";
+    public static staging2 : string = "https://random.com/login";
+    public static staging3 : string = "https://random2.com/login";
+    public static production : string = "https://random3.com/login";
 
 }
